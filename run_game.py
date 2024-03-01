3bff6d44b98555143fffa359e79e5fc6a70d4ef0
higher-bottle <bingting.hf2001@outlook.com>
2024-03-02 01:38:32 +0800

    commit run_game

---
+++ run_game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 01:25:17 2024
+
+@author: bingtinghuangfu
+"""
+
+import alien_invation as AlienInvasion
+
+ai= AlienInvasion.AlienInvasion()
+ai.run_game()
